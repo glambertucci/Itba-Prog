27f98200b2e3ebb90cd5a7e14aed1d0b82f76b26
Guido <glambertucci@itba.edu.ar>
2017-08-23 10:47:44 -0300

    TP02

---
+++ TP02_K4/TP02_A.c
@@ -0,0 +1,61 @@
+//TP PRACTICO N°2. Bloques de control
+/*Fecha de creacion 23/8/2017
+Grupo 4
+Integrantes:
+Lambertucci Guido	58009
+Hrubisiuk 	Agustin	58311
+Mechoulam	Alan	58438
+Moriconi	Franco	58495
+Este programa tomara el numero ingresado a CANDIDATO e imprimira en pantalla
+si el mismo es primo o compuesto.
+Nota 1: El programa no aceptará numeros mayores a 15000000.
+Nota 2: Numeros negativos no son validos.
+Nota 3: El numero 1 no es considerado primo ni compuesto.
+*/
+
+
+#include <stdio.h>
+#define CANDIDATO 104597 
+
+int main (void)
+{
+	int numero_a_evaluar=CANDIDATO;		
+	int divisor,cantidad_de_divisores=0;
+
+//Verificaciones de entrada
+	if (numero_a_evaluar>15000000)		
+	{
+		printf("El numero es demasiado grande, ingrese otro\n");
+	}	
+	else if (numero_a_evaluar<1)
+	{
+		printf("El numero ingresado no es valido, vuelva a modificarlo\n");
+	}
+	else if (numero_a_evaluar==1)
+	{
+		printf("El numero %d no es primo ni compuesto\n",numero_a_evaluar);
+	}
+	//Una vez pasadas las verificaciones procede a analizar el numero
+	//Basicamente divide el numero por todos los menores a el hasta el 1
+	//como esto para numeros muy alto se hace muy largo decidimos acotar el numero
+	//si el numero tiene mas de dos divisores automaticamente no es primo
+	else 
+	{	
+		for(divisor=1;(divisor<=numero_a_evaluar) || (cantidad_de_divisores==3);divisor++)
+		{
+			if((numero_a_evaluar%divisor)==0)
+			{
+				cantidad_de_divisores++;
+			}
+		}
+		if(cantidad_de_divisores==2)
+		{
+			printf("El numero %d es primo\n",numero_a_evaluar);
+		}
+		else 
+		{
+			printf("El numero %d es compuesto\n",numero_a_evaluar );
+		}
+	}
+	return 0;
+}	
---
+++ TP02_K4/TP02_B.c
@@ -0,0 +1,39 @@
+//TP PRACTICO N°2. Bloques de control
+/*Fecha de creacion 23/8/2017
+Grupo 4
+Integrantes:
+Lambertucci Guido	58009
+Hrubisiuk 	Agustin	58311
+Mechoulam	Alan	58438
+Moriconi	Franco	58495
+Este programa imprimirá una matriz de n x m (fil, col). Imprimira en pantalla
+una matriz de números crecientes de izquierda a derecha y de arriba hacia abajo
+*/
+#include <stdio.h>
+#define FILAS 5
+#define COLUMNAS 5		//define que se usa para asignar las variables
+
+int main (void)
+{
+	unsigned int fil=FILAS,col=COLUMNAS;	//no signado asi ahorro el problema de numeros negativos
+	int aux_col,aux_cont=0;					//variables usadas para imprimir en pantalla
+	int screen;								//numero que ira a pantalla
+	
+	if ((col>25) || (fil>25))
+	{
+		printf("El numero de fila o columna es muy grande\n");
+	}
+
+	else while (fil--)
+	{	
+		aux_col=col;
+		screen=aux_cont++;
+		while(aux_col--)				//contador para aumentar el numero de fila a fila
+		{
+			printf("%d\t",screen++);	//imprime las columnas luego las filas, usa un 
+					
+		}
+		printf("\n");
+	}
+	return 0;
+}
