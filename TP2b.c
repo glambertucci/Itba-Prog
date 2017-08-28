79a9b40d38aedaacc984ff472686aa64c8a6bee3
glambertucci <glambertucci@itba.edu.ar>
2017-08-27 22:02:47 -0300

    Esta todo bien, cambie solo una boludez de un printf por el define

--- TP2b.c
+++ TP2b.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define FILAS 3
-#define COLUMNAS 3
+#define FILAS 9
+#define COLUMNAS 9
 #define MAX 20	//Limite de dimension de matriz
 
 int main(void)
@@ -9,14 +9,14 @@ int main(void)
 	int fil, col, auxfil, auxcol;
 
 	if(COLUMNAS>MAX||FILAS>MAX||FILAS<-MAX||COLUMNAS<-MAX)	//Comprueba que la dimension no exceda el limite
-		printf("ERROR: Dimension de la matriz excede el limite. (Limite = 20)\n");
+		printf("ERROR: Dimension de la matriz excede el limite. (Limite = %d)\n",MAX);
 	else if(COLUMNAS==0||FILAS==0)	//Comprueba que la dimension no sea cero
 		printf("ERROR: La dimesion de la matriz no debe ser cero.\n");
 	else
 	{
 		if(FILAS<0)	//Comprueba que la dimension no sea negativa, si lo es, tira warning
 		{
-			printf("WARNING: No se aceptan numeros negativos.\nSe tomara el numero de filas positivo\n");	//Creo que quedaria mejor solo puteando al usuario y no tomar el positivo
+			printf("WARNING: No se aceptan numeros negativos.\nSe tomara el numero de filas positivo\n");	
 			auxfil=-FILAS;
 		}
 		else
@@ -34,7 +34,7 @@ int main(void)
 
 		for(fil=0;fil<auxfil;fil++)	//Algoritmo de creacion de matriz, itera el primer for por cada fila
 		{
-			for(col=fil;col<auxcol+fil;col++)	//y luego itera por cada numero en las columnas
+			for(col=fil;col<(auxcol+fil);col++)	//y luego itera por cada numero en las columnas
 			{
 				printf("%d\t", col);
 			}
