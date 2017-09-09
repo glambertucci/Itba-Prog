7c4d23e8b3278fcbabd21641f9957df54b48e962
Guido <glambertucci@itba.edu.ar>
2017-09-09 11:44:57 -0300

    puse el delay y el printmat

--- TP04_K4/screen.c
+++ TP04_K4/screen.c
@@ -1,3 +1,31 @@
+#include <stdio.h>
+#define M 6 // Cantidad de filas
+#define N 6
+#define TIME 88889999
+void welcomeMsg (void);
+void printMatrix(char mat[M][N]);
+void delay (void);
+
+char seed_matrix [M][N] = {
+	{' ',' ',' ',' ',' ',' '},
+	{' ','*','*','*','*',' '},
+	{' ','*','*','*',' ',' '},
+	{' ','*','*','*','*',' '},
+	{' ','*',' ',' ',' ',' '},
+	{' ',' ','*','*',' ',' '}
+};
+
+
+int main (void)
+{
+	welcomeMsg();
+	delay();
+	printMatrix (seed_matrix);
+	delay();
+	printf("puto\n");
+	return 0;
+
+}
 void welcomeMsg (void)
 {
 	printf("Bienvenido al juego de la vida \n");
@@ -8,3 +36,28 @@ void welcomeMsg (void)
 	printf("toque cualquier tecla\n");
 }
 
+
+void printMatrix(char mat[M][N]){
+	int i, j;
+	printf("\n");
+	for (i = 0;i < N;i++){
+		for (j = 0;j < M;j++){
+			printf("|%c",mat[i][j]);
+		}
+		printf("|\n");
+	}
+	printf ("\n");
+}
+void delay (void){
+int a;
+int b=a=TIME;
+
+	while(a)	//perdiendo el timepo
+	{
+		while(b)
+		{
+			b--;
+		}
+		a--;
+	}
+}
