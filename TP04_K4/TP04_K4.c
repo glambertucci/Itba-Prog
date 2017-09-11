a12577902360ca35eb9dc910b0b0af33cd4ce28d
amechu <amechoulam@itba.edu.ar>
2017-09-10 22:40:54 -0300

    hola

--- TP04_K4/TP04_K4.c
+++ TP04_K4/TP04_K4.c
@@ -138,35 +138,7 @@ int cellStatus (int m, int n, char matrix[HEIGHT][WIDTH])
       char current_cell = 0;
       int neighbour_alive = 0;
       int destiny = 0;
-      int i=0;
-      int upperrow = m-OFFSET;
-      int bottomrow = m+OFFSET;
 
-/*
-	if (matrix [m-OFFSET] [n-OFFSET] == ALIVE)	//analizo la celula diagonal superior izquierda
-		neighbour_alive++;
-	
-	if (matrix [m-OFFSET] [n] == ALIVE)	//analizo la celula superior central
-		neighbour_alive++;
-	
-	if (matrix [m-OFFSET] [n+OFFSET] == ALIVE)	//analizo la celula diagonal superior derecha 
-		neighbour_alive++;
-	
-	if (matrix [m] [n-OFFSET] == ALIVE)	//analizo la celula izquierda
-		neighbour_alive++;
-	
-	if (matrix [m] [n+OFFSET] == ALIVE)	//analizo la celula derecha
-		neighbour_alive++;
-	
-	if (matrix [m+OFFSET] [n-OFFSET] == ALIVE)	//analizo la celula diagonal inferior izquierda
-		neighbour_alive++;
-	
-	if (matrix [m+OFFSET] [n] == ALIVE)	//analizo la celula inferior central
-		neighbour_alive++;
-	
-	if (matrix [m+OFFSET] [n+OFFSET] == ALIVE)	//analizo la celula diagonal inferior derecha
-		neighbour_alive++;
-	*/
       current_cell = matrix [m] [n];   //estado actual de la celula, Dead o Alive
 	if (matrix [m-OFFSET] [n-OFFSET] == ALIVE)	//analizo la celula diagonal superior izquierda
 		neighbour_alive++;
