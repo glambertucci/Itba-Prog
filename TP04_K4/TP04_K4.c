c182925978bb9da781436474079213d3855232ab
Guido <glambertucci@itba.edu.ar>
2017-09-13 10:53:46 -0300

    empezando ese famoso tp5

--- TP04_K4/TP04_K4.c
+++ TP04_K4/TP04_K4.c
@@ -1,3 +1,25 @@
+//Trabajo Práctico n°4  
+/*Fecha de Creación: 08/09/2017
+Grupo 4
+Integrantes:
+Lambertucci Guido	58009
+Hrubisiuk 	Agustin	58311
+Mechoulam	Alan	58438
+Moriconi	Franco	58495
+
+
+ El Juego de la Vida
+Este programa muestra una matriz en pantalla que contiene células "vivas" y "muertas".
+Las mismas bajo ciertas condiciones sobreviven, mueren o reviven.
+Criterio a tener en cuenta: Se toma una matriz de tamaño nxn pero que de la cual 
+realmente se mostrará (n-2)x(n-2). Esto a nivel de programación se realiza para que 
+la función encargada de realizar el chequeo de los estados de las células colindantes 
+no haga la verificación de estado con memoria basura o celulas no colindantes en los "bordes" de la matriz. 
+Este programa tiene hardcodeado las celulas vivas del juego. Estas pueden ser modificadas,
+ eliminadas o agregadas al mapa inicial, siempre y cuando respete la dimensión de la matriz escogida.
+  Dicha función solo inicializa la primer matriz del juego.
+En caso de querer finalizar toque ESC y enter, un enter avanza de generacion y si ingresa un numero avanza las generaciones ingresadas 
+*/
 #include <stdio.h>
 
 #define DEAD ' '
