98a49d15412df2f3e046fa7d4f2f9169c6da10bb
Rubidio24 <31071918+Rubidio24@users.noreply.github.com>
2017-10-08 22:11:56 -0300

    Update Keyboard.h

--- TP07_K4 branch/Back End/Keyboard.h
+++ TP07_K4 branch/Back End/Keyboard.h
@@ -1,6 +1,11 @@
 #ifndef Keyboard_H
 #define Keyboard_H
 
+/*
+ * Este header contiene el prototipo de la función encargada de obtener los datos introducidos por el
+ *usuario mediante teclado.
+ */
+
 int readNumber(void);
 
 #endif
