35b15fa645d2312c424a7cf137f815c098dffe10
Guido <glambertucci@itba.edu.ar>
2017-10-13 16:38:09 -0300

    falta lo que esta comentado

--- TP08_K4/main.c
+++ TP08_K4/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define GREEN   "\x1b[32m"
 #define BLUE    "\x1b[34m"
 #define COLOR_RESET   "\x1b[0m"
- double calcres (double num1, double num2, char op);
-
+ double calcRes (double num1, double num2, char op);
+ int addOperation(char op, double (*functions) (double num1 ,double num2));
 char operands[7]={'+','-','*','/','|','&','^'};
 //double (*(functions[7])) (double,double);
 
@@ -12,26 +13,53 @@ char operands[7]={'+','-','*','/','|','&','^'};
 int main (void)
 {
 	double num1,num2,result;
-	char arr[1000];
-	char * pnum2;
+	char arr[1000],trash;
+	char * pnum2, * find;
 
-	printf("Bienvenido a la calculadora \nLas funciones basicas estan habilitadas incluyendo AND OR y XOR \n(siendo estas exclusivas para numeros enteros)\nIngrese Numero Operador Numero y obtendra el resultado\n");
+	printf("Bienvenido a la calculadora \n");//Funciones
+	printf("Las funciones basicas estan habilitadas incluyendo AND OR y XOR \n");
+	printf("(siendo estas exclusivas para numeros enteros)\n");
+	printf("Ingrese Numero Operador Numero y obtendra el resultado\n");
+	printf("Considere que el formato es [NUMERO] [OPERADOR] [NUMERO] (sin los corchetes)\n");
+	printf("y se tomaran los dos primeros numeros, el resto será ignorado\n");
 	while (abort)
 	{
 //		mode();// va a ser funcion que preguntra el modo al inicio  y luego pregunte si quiere mantener, cambiarlo  o q para salir 
-//		initFun();//inicia funciones
+//		addOperation();//inicia funciones
 		scanf("%lf%s", (&num1),arr);
-		pnum2 = & arr[1];
+		pnum2 = &arr[1];
 		char op=arr[0];
-						//fijate si op es valido
-		num2= atof(pnum2);
-		//aca te fijas si es valido num2 	
-		result=	calcres(num1,num2,op);
-		printf(""GREEN"Cuenta"COLOR_RESET": %lg %c % lg \n" BLUE"Resultado" COLOR_RESET ": % lg\n",num1,op,num2,result );
+	
+		find =	strchr(operands, op);
+		if (find == NULL)	//ACA VERIFIQUE SI OP ES VALIDO
+		{
+			printf("Entrada invalida\n");
+		}
+		else 
+		{
+			num2= atof(pnum2); 
+			if (((arr[1]< '0' ) || (arr[1]>'9') ) && (arr[1] != '.'))
+			{
+				printf("Entrada invalida\n");
+			}
+			else
+			{
+				if (num2==0 && op == operands[3])
+				{
+					printf("Indefinido\n");
+				} 	
+				else 
+				{
+					result=	calcRes(num1,num2,op);
+					printf(""GREEN"Cuenta"COLOR_RESET": %lg %c % lg \n" BLUE"Resultado" COLOR_RESET ": % lg\n",num1,op,num2,result );
+				}
+			}
+		}
+		while(trash=getchar() != '\n');//Limpio el buffer Esto deberia ser una funcion
 	}
 }
 
-double calcres (double num1, double num2, char op)
+double calcRes (double num1, double num2, char op)
 {
 	double result;
 	if (op==operands[0])
