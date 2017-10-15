b51f2cfb4b945a1f55bfd0c27a7c7cb5527143a7
Guido <glambertucci@itba.edu.ar>
2017-10-15 19:53:10 -0300

    estuve agregando punteros a funcion

--- TP08_K4/main.c
+++ TP08_K4/main.c
@@ -5,10 +5,19 @@
 #define GREEN   "\x1b[32m"
 #define BLUE    "\x1b[34m"
 #define COLOR_RESET   "\x1b[0m"
- double calcRes (double num1, double num2, char op);
- int addOperation(char op, double (*functions) (double num1 ,double num2));
-char operands[7]={'+','-','*','/','|','&','^'};
-//double (*(functions[7])) (double,double);
+double xorr (double num1,double  num2);
+double summ (double num1,double num2);
+double ress (double num1,double num2);
+double mull (double num1,double num2);
+double divi (double num1,double num2);
+double andd (double num1,double num2);
+double orr (double num1,double  num2);
+
+
+double calcRes (double num1, double num2, char op);
+int addOperation(int mode);
+char operands[7];
+double (* functions [7]) (double, double);
 
 
 int main (void)
@@ -29,12 +38,13 @@ int main (void)
 	{
 
 		modo = modeAsk();
-//		addOperation();//inicia funciones
-
+		printf("%d\n",modo );
+		addOperation(modo);//inicia funciones
+		printf("%c%c%c%c%c%c%c\n",operands[0],operands[1],operands[2],operands[3],operands[4],operands[5],operands[6] );
 		scanf("%lf%s", (&num1),arr);
 		pnum2 = &arr[1];
 		char op=arr[0];
-	
+		
 		find =	strchr(operands, op);
 		if (find == NULL)	//ACA VERIFIQUE SI OP ES VALIDO
 		{
@@ -68,18 +78,81 @@ double calcRes (double num1, double num2, char op)
 {
 	double result;
 	if (op==operands[0])
-		result = num1+num2;
+		result = functions[0] (num1,num2);
 	if (op==operands[1])
-		result = num1-num2;
+		result = functions[1] (num1,num2);
 	if (op==operands[2])
-		result = num1*num2;
+		result = functions[2] (num1,num2);
 	if (op==operands[3])
-		result = num1/num2;
+		result = functions[3] (num1,num2);
 	if (op==operands[4])
-		result = (int)num1 | (int)num2;
+		result = functions[4] ((int) num1, (int) num2);
 	if (op==operands[5])
-		result = (int)num1 & (int)num2;
+		result = functions[5] ((int) num1,(int) num2);
 	if (op==operands[6])
-		result = (int)num1 ^ (int)num2;	
+		result = functions[6] ((int) num1,(int) num2);	
 	return result;
+}
+
+int addOperation(int mode )
+{
+	char classic[7]={'+','-','*','/','&','|','^'};
+	char letters[7]={'s','r','m','d','a','o','x'};
+	int index;
+	if (mode == 2)
+	{
+		for (index=0;index<7;index++)
+		{
+			operands[index]=classic[index];
+		}
+	}
+	else 
+		for (index=0;index<7;index++)
+		{
+			operands[index]=letters[index];
+		}
+
+	double (* sum_pointer) (double, double)= &summ;
+	double (* res_pointer) (double, double)=&ress;
+	double (* mul_pointer) (double, double)=&mull;
+ 	double (* div_pointer) (double, double)=&divi;
+	double (* and_pointer) (double, double)=&andd;
+	double (* or_pointer) (double, double)=&orr;
+	double (* xor_pointer) (double, double)=&xorr;
+
+	functions[0] = sum_pointer;
+	functions[1] = res_pointer;
+	functions[2]= mul_pointer;
+	functions[3]= div_pointer;
+	functions[4]= and_pointer;
+	functions[5]= or_pointer;
+	functions[6]= xor_pointer;
+}
+double summ (double num1,double num2)
+{
+	return num1+num2;
+}
+double ress (double num1,double num2)
+{
+	return num1-num2;
+}
+double mull (double num1,double num2)
+{
+	return num1*num2;
+}
+double divi (double num1,double num2)
+{
+	return num1/num2;
+}
+double andd (double num1,double num2)
+{
+	return (int) num1& (int) num2;
+}
+double orr (double num1,double  num2)
+{
+	return (int)  num1|(int) num2;
+}
+double xorr (double num1,double  num2)
+{
+	return (int)  num1^ (int) num2;
 }
