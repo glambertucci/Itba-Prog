e26db3702166bc3e2183c6ed7c6f6adec4b20c44
amechu <amechoulam@itba.edu.ar>
2017-10-15 21:32:00 -0300

    h

--- TP08_K4/main.c
+++ TP08_K4/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "calcfunc.h"
-#include "keyboard.h"
-#include "colordefines.h"
+#include "Main.h"
+#include "CalcFunc.h"
+#include "KeyboardUtilities.h"
+#include "Messages.h"
+#include "ModeSelection.h"
+
 char operands[7]={'+','-','*','/','|','&','^'};
 double (* functions [7]) (double, double);
 
@@ -15,12 +18,8 @@ int main (void)
 	char * pnum2, * find;
 	int modo, abort = 1;
 
-	printf("Bienvenido a la calculadora \n");//Funciones
-	printf("Las funciones basicas estan habilitadas incluyendo AND OR y XOR \n");
-	printf("(siendo estas exclusivas para numeros enteros)\n");
-	printf("Ingrese Numero Operador Numero y obtendra el resultado\n");
-	printf("Considere que el formato es [NUMERO] [OPERADOR] [NUMERO] (sin los corchetes)\n");
-	printf("y se tomaran los dos primeros numeros, el resto será ignorado\n");
+	welcomeMessage();
+
 	while (abort)
 	{
 		if ((modo = modeAsk()) == 0)
@@ -35,30 +34,30 @@ int main (void)
 		find =	strchr(operands, op);
 		if (find == NULL)	//ACA VERIFIQCA SI EL OPERANDO ES VALIDO
 		{
-			printf("Entrada invalida\n");
+			invalidMessage();
 		}
 		else 
 		{
 			num2= atof(pnum2); 
 			if (((arr[1]< '0' ) || (arr[1]>'9') ) && (arr[1] != '.'))
 			{
-				printf("Entrada invalida\n");
+				invalidMessage();
 			}
 			else
 			{
 				if (num2==0 && op == operands[3])
 				{
-					printf("Indefinido\n"); //Dividir por 0 es malo, y un conejito muere cada vez que lo intentas.
+					indefiniteMessage(); //Dividir por 0 es malo, y un conejito muere cada vez que lo intentas.
 				} 	
 				else 
 				{
 					result=	calcRes(num1,num2,op);
-					printf(""GREEN"Cuenta"COLOR_RESET": %lg %c % lg \n" BLUE"Resultado" COLOR_RESET ": % lg\n",num1,op,num2,result );
+					answerMessage(num1, op, num2, result);
 				}
 			}
 		}
 		cleanbuffer();//Limpio el buffer
 	}
-	printf("Terminando programa..\n\n");
+	endMessage();
 	return 0;
 }
---
+++ tp8 branch/Main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Main.h"
+#include "CalcFunc.h"
+#include "KeyboardUtilities.h"
+#include "Messages.h"
+#include "ModeSelection.h"
+
+char operands[7]={'+','-','*','/','|','&','^'};
+double (* functions [7]) (double, double);
+
+int main (void)
+{
+	double num1,num2,result;
+
+	char arr[1000];
+	char * pnum2, * find;
+	int modo, abort = 1;
+
+	welcomeMessage();
+
+	while (abort)
+	{
+		if ((modo = modeAsk()) == 0)
+			abort = 0;
+		printf("%d\n",modo );
+		addOperation(modo);//inicia funciones
+		printf("%c%c%c%c%c%c%c\n",operands[0],operands[1],operands[2],operands[3],operands[4],operands[5],operands[6] );
+		scanf("%lf%s", (&num1),arr);
+		pnum2 = &arr[1];
+		char op=arr[0];
+		
+		find =	strchr(operands, op);
+		if (find == NULL)	//ACA VERIFIQCA SI EL OPERANDO ES VALIDO
+		{
+			invalidMessage();
+		}
+		else 
+		{
+			num2= atof(pnum2); 
+			if (((arr[1]< '0' ) || (arr[1]>'9') ) && (arr[1] != '.'))
+			{
+				invalidMessage();
+			}
+			else
+			{
+				if (num2==0 && op == operands[3])
+				{
+					indefiniteMessage(); //Dividir por 0 es malo, y un conejito muere cada vez que lo intentas.
+				} 	
+				else 
+				{
+					result=	calcRes(num1,num2,op);
+					answerMessage(num1, op, num2, result);
+				}
+			}
+		}
+		cleanbuffer();//Limpio el buffer
+	}
+	endMessage();
+	return 0;
+}
