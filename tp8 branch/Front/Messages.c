411a04391b86105ac12dbd126b4b04ddfe7d9afe
glambertucci <glambertucci@itba.edu.ar>
2017-10-15 22:02:49 -0300

    sinataxis!!!!!!!!!!!!!!!!!!!!

--- tp8 branch/Front/Messages.c
+++ tp8 branch/Front/Messages.c
@@ -12,17 +12,17 @@ void welcomeMessage(void);
 	printf("y se tomaran los dos primeros numeros, el resto será ignorado\n");
 }
 
-void invalidMessage(void);
+void invalidMessage(void)
 {
 	printf("Entrada invalida.\n");
 }
 
-void indefiniteMessage(void);
+void indefiniteMessage(void)
 {
 	printf("Indefinido.\n");
 }
 
-void answerMessage(double num1, char op, double num2, double result);
+void answerMessage(double num1, char op, double num2, double result)
 {
 	printf(""GREEN"Cuenta"COLOR_RESET": %lg %c % lg \n" BLUE"Resultado" COLOR_RESET ": % lg\n",num1,op,num2,result );
 }
