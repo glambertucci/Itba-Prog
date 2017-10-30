6c720e0d32a54357301a54017696279c89394c4b
amechu <amechoulam@itba.edu.ar>
2017-10-29 22:40:19 -0300

    asg

--- TP09_K4/TerminalFront.c
+++ TP09_K4/TerminalFront.c
@@ -40,20 +40,6 @@ void message(uint8_t state)
 
 }
 
-uint8_t* bitArrayInit (void)
-{
-	static uint8_t bitArray[8] = {0};
-
-	return (uint8_t*) bitArray;
-}
-
-void updateBitArray (uint8_t * array)
-{
-	uint8_t i;
-
-	for(i = 0; i < 8; i++)
-		(*(array+i) = (getBitValue(PORTA, i)));
-}
 
 void updateScreen(uint8_t bitArray[8], uint8_t state)
 {
@@ -61,7 +47,6 @@ void updateScreen(uint8_t bitArray[8], uint8_t state)
 	printPort(bitArray);
 	message(state);
 }
-
 
 void blinkFunction(uint8_t * array, uint8_t state)
 {
