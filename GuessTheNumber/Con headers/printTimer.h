902844d99ab1350d6bcb393e4f9e90b43509c67f
Rubidio24 <31071918+Rubidio24@users.noreply.github.com>
2017-11-05 23:12:45 -0300

    Delete printTimer.h

--- GuessTheNumber/Con headers/printTimer.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINTTIMER_H
-#define PRINTTIMER_H
-
-void printTimer (int time);
-//Esta función imprime en pantalla el tiempo restante que le queda al usuario para hallar el número correcto
-
-
-#endif /* PRINTTIMER_H */
-
