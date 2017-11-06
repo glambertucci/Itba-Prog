6f5a99c5512e2dc314f0e28e60c97f4389b5bc81
Rubidio24 <31071918+Rubidio24@users.noreply.github.com>
2017-11-05 23:44:11 -0300

    Add files via upload

--- GuessTheNumber/Con header/printScreen.h
+++ GuessTheNumber/Con header/printScreen.h
@@ -6,4 +6,13 @@ void printScreen (int state, int time);
 //el tiempo restante.
 
 
+void printTimer (int time);
+//Esta función imprime en pantalla el tiempo restante que le queda al usuario para hallar el número correcto
+
+
+void printText (int state);
+//Esta función es la encargada de imprimir en pantalla los mensajes requeridos por el usuario para la comprensión del programa
+//(Instrucciones, "castigos", si ganó o perdió)
+
+
 #endif /* PRINTSCREEN_H */
