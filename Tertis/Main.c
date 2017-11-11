6b4c4b1e8ad571fb8c68210624d5d5deb77ecf72
glambertucci <glambertucci@itba.edu.ar>
2017-11-11 20:53:51 -0300

    Add files via upload

--- Tertis/Main.c
+++ Tertis/Main.c
@@ -1,3 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Main.c
+ * Author: alan
+ *
+ * Created on November 10, 2017, 3:11 PM
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -6,9 +19,16 @@ int main(void) {
     
     PIECE pieces[PIECE_TOTAL]; //Arreglo de todas las piezas
     
-    PIECE matrix[TABLE_FIL][TABLE_COL]; //Matriz en donde se situan las piezas
+    PIECE matrix[MATRIX_WIDTH][MATRIX_HEIGHT]; //Matriz en donde se situan las piezas
+    
+  //PIECE auxmatrix[MATRIX_WIDTH + 2][MATRIX_HEIGHT + 2]; //Matriz con la cual trabajamos
     
     tetrisInit(pieces, matrix); //Aca se inicializan las piezas y la matriz.
+    
+    
+    
+    
+    
     
     
 }
