196b8687227356804e2003daf8f1c88c455f959f
amechu <amechoulam@itba.edu.ar>
2017-11-13 09:34:56 -0300

    subido tp11 roto por mergeado de mierda de git

---
+++ tp11 arreglado/config.h
@@ -0,0 +1,45 @@
+
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define BASE_FONT "isocpeur.ttf"
+#define BASE_SIZE 50
+#define SCREEN_W 1000
+#define SCREEN_H 800
+    
+#define FPS 60.0
+#define GAME_TIME 5
+#define MAX_NUMBER 10
+    
+#define CENTER_X 460
+#define CENTER_Y 430
+#define TRIANGLE_SIZE 600
+    
+#define DEFAULT_DIRECTION 180  
+    
+#define DEFAULT_RED 0
+#define DEFAULT_BLUE 0.3f
+#define DEFAULT_GREEN 0.3f
+
+#define DEFAULT_TOL 2.0
+#define DEFAULT_CNT_FRACT 5
+    
+#define MIN_TOL 0
+#define MAX_TOL 200
+#define MAX_COMPUTED_LVL 6
+    
+#define DEF_SZ 50 // default auxiliar text size
+    
+#define FREQ_FRACTAL 1 // seconds
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONFIG_H */
+
---
+++ tp11 arreglado/read_command.h
@@ -0,0 +1,18 @@
+
+#ifndef READ_COMMAND_H
+#define READ_COMMAND_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "front_end.h"
+    
+int read_input(int argc,char *argv[] , shared_vars_t* shared_vars);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* READ_COMMAND_H */
+
