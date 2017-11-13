6d9ed70777445b39d18c8ffb561e5e0818cd5fa8
amechu <amechoulam@itba.edu.ar>
2017-11-13 00:22:30 -0300

    aniadido el rotate

--- Tertis/rotate.c
+++ Tertis/rotate.c
@@ -2,11 +2,13 @@
 
 void rotate(PIECE matrix[TABLE_FIL][TABLE_COL]) {
     
-    uint8_t pivot_fil, pivot_col, i, j;
+    uint8_t pivot_fil, pivot_col, i, j, k=0, temp_type;
+    uint8_t temp_fil[4], temp_col[4];
     
     for(i = 2; i < TABLE_FIL-2; i++){
         for(j = 2; j < TABLE_COL-2; j++){
             if((matrix[i][j].pivot == true) && (matrix[i][j].state == CAYENDO)){
+                temp_type = matrix[i][j].type;
                 pivot_fil = i;
                 pivot_col = j; //solamente va a haber UN UNICO pivote cayendo siempre
             }   
@@ -19,12 +21,19 @@ void rotate(PIECE matrix[TABLE_FIL][TABLE_COL]) {
     for(i = pivot_fil-2; i < pivot_fil+2; i++){
         for(j = pivot_col-2; j < pivot_col+2; j++){
             if(matrix[i][j].state == CAYENDO){  //En la linea de abajo coloco el cuadradito nuevo
-                matrix[(pivot_fil)-(j-pivot_col)][(pivot_col)+(i-pivot_fil)].type = matrix[i][j].type;
-                if(!(pivot_fil == i && pivot_col == j)){
-                    matrix[i][j].type = BLANK;      //Aca borro
-                    matrix[i][j].state = ESTATICO;  //el original
-                }
-            }          
-        }
+                *(temp_fil + k) = ((pivot_fil)+(j-pivot_col));
+                *(temp_col + k) = ((pivot_col)-(i-pivot_fil));
+                matrix[i][j].type = BLANK;      //Aca borro
+                matrix[i][j].state = ESTATICO;  //el original
+                k++;
+                matrix[(pivot_fil)+(j-pivot_col)][(pivot_col)-(i-pivot_fil)].type = matrix[i][j].type;
+                matrix[i][j].type = BLANK;      //Aca borro
+                matrix[i][j].state = ESTATICO;  //el original
+            }
+        }          
+    }               
+    for(i = 0; i < 4; i++){
+        matrix[*(temp_fil + i)][*(temp_col + i)].type = temp_type;
     }
+
 }
