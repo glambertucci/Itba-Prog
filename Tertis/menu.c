959e17d9cc223788813ba3ece3d54e2854475df5
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2017-11-29 19:53:38 -0300

    the menu is back
    yay

--- Tertis/menu.c
+++ Tertis/menu.c
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 #include <allegro5/bitmap.h>
 #include <allegro5/display.h>
 #include <allegro5/bitmap_io.h>
@@ -13,30 +8,59 @@
 #include <allegro5/allegro_image.h> 
 #include <allegro5/allegro_acodec.h>
 #include <allegro5/allegro_audio.h>
-
-
+#include <allegro5/allegro.h>
 #include "menu.h"
-int menu (void)
-{
-    ALLEGRO_BITMAP * symbol;
-    al_init_image_addon();
-    
-    al_clear_to_color(al_map_rgb(0,0,0));
-    
-     symbol=   al_create_bitmap(800,800);
 
+
+int menu (ALLEGRO_DISPLAY * display, ALLEGRO_EVENT_QUEUE *event_queue){
+
+    
+  
+   ALLEGRO_BITMAP * image; //PUntero a la imagen del menu
+   int option = -1; //Esta variable va al return, indicando qué se presionó.
+   
+ //Registro los eventos de display y de mouse.
+   
+   image = al_load_bitmap("menu.jpg");
+   al_draw_bitmap(image,0,0,0);
+   al_flip_display();
+   
+   while(option == -1)
+   {
+      ALLEGRO_EVENT ev;
+      al_wait_for_event(event_queue, &ev);
       
-    if (!(symbol= al_load_bitmap("menu.jpg") ))
-    {
-        fprintf(stderr, "Image not loaded");
-        return -1;
-    }  
-    //aca deberia haber un while, que sea hasta que elija una de las opciones, estas siendo 4 que es close display quit continue y start, quit y close display devuelven QUIT
-     //los otros segun el enum, para fijarse como detectar los click del mouse miren allegro.cc o el tp anterior, pero ahi esta hehco medio negro, encontre nuevas cosas en allegro,.cc que estn piola
-     al_draw_bitmap(symbol,0,0,0);
-     al_flip_display();
-     al_rest(3.0);
+      if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+         break;
+      } // Si se cierra el display, se corta el while.
+      
+      
+      /* Estos else if cambian el valor de  la variable opcion cuando 
+       * haga click en las coordenadas de START, CONTINUE o QUIT */
+      
+      else if((ev.mouse.x > MENUMARGIN && ev.mouse.y > (STARTPOSY)) &&
+              (ev.mouse.x < (STARTPOSXEND) && ev.mouse.y < (STARTPOSYEND))){
+          if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+              option = START;}
+      } 
+      
+      else if((ev.mouse.x > MENUMARGIN && ev.mouse.y > (CONTPOSY)) &&
+              (ev.mouse.x < (CONTPOSXEND) && ev.mouse.y < (CONTPOSYEND))){
+          if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+              option = CONTINUE;
+          }
+       }
+
+      else if((ev.mouse.x > MENUMARGIN && ev.mouse.y > QUITPOSY) &&
+          (ev.mouse.x < QUITPOSXEND && ev.mouse.y < QUITPOSYEND))
+          if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+              option = QUIT;}
      
- al_destroy_bitmap(symbol);
-    return 0;
+   }
+ 
+   
+   al_destroy_display(display); //Cierro el display y la cola de eventos.
+   al_destroy_event_queue(event_queue); //Pues estaré cerrando el juego.
+    
+ return option;
 }
