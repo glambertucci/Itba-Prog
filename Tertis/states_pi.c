9a1c03b7cede12b2710cb4c35d2402c0a2f8d0cc
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2017-12-14 22:35:23 -0300

    TIMER

--- Tertis/states_pi.c
+++ Tertis/states_pi.c
@@ -1,23 +1,21 @@
 #include <time.h>
 #include "general.h"
 
-
-/* void event_routine(EV_UTILS * ev_utils){
+void event_routine(EV_UTILS * ev_utils){
     
-    while(!(ev_utils->ev_end)){
-        static clock_t timezero;
-        clock_t timedelta;
-        long double msec;
+    while(!(ev_utils->ev_end)){ //EL while terminará cuando se cierren los eventos.
+        static clock_t timezero; //Empieza el timer en un valor.
+        clock_t timedelta; //Diferencia de tiempo en ticks del procesador.
+        long double msec; //Diferencia de tiempo en segundos (con precisión de milisegundos)
         
-        if(!(ev_utils->pause)){
+        if(!(ev_utils->pause)){   //Si el timer no está pausado, está andando.
+            timedelta = (clock() - timezero); //La diferencia de tiempo es el tiempo actual menos el inicial.
+            msec = timedelta/(float) (CLOCKS_PER_SEC); //Lo convertimos a segundos.
             
-            
-            timedelta = (clock() - timezero);
-            msec = timedelta/(CLOCKS_PER_SEC/1000);
-            if( timedelta >= ev_util->timer_speed){
-                ev_utils->timer_event = 1;
-                timezero = clock();
+            if( msec >= ev_util->timer_speed){ //Comparamos esta coversión al tiempo en el cual debe tirar eventos.
+                ev_utils->timer_event = 1; //Si lo superó, indicamos un evento de timer.
+                timezero = clock(); //Finalmente, seteamos el tiempo actual como el nuevo inicial 
             } 
         }    
     }
-}*/
+}
